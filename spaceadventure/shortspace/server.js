17e50fbfc97cd889a3ac822cd6bce9c0e44e5fe4
rkrug5 <phishermankrug@hotmail.com>
2018-05-08 14:01:51 -0700

    progress

--- spaceadventure/shortspace/server.js
+++ spaceadventure/shortspace/server.js
@@ -47,9 +47,35 @@ app.get("/result1", function (req, res) {
 app.get("/result2", function (req, res) {
 	res.sendFile(path.join(__dirname, "public/result2.html"));
 });
+
 app.get("/choice2", function (req, res) {
 	res.sendFile(path.join(__dirname, "public/choice2.html"));
 });
+
+app.get("/result3", function (req, res) {
+	res.sendFile(path.join(__dirname, "public/result3.html"));
+});
+app.get("/result4", function (req, res) {
+	res.sendFile(path.join(__dirname, "public/result4.html"));
+});
+
+app.get("/choice3", function (req, res) {
+	res.sendFile(path.join(__dirname, "public/choice3.html"));
+});
+
+app.get("/choice4", function (req, res) {
+	res.sendFile(path.join(__dirname, "public/choice4.html"));
+});
+
+app.get("/result5", function (req, res) {
+	res.sendFile(path.join(__dirname, "public/result5.html"));
+});
+
+
+app.get("/result6", function (req, res) {
+	res.sendFile(path.join(__dirname, "public/result6.html"));
+});
+
 
 app.get("/conclusion", function (req, res) {
 	res.sendFile(path.join(__dirname, "app/public/conclusion.html"));
